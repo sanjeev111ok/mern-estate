b4a631280dda3b84246ff1aa7168bd51c0f53bd6
Sanjeev Shrestha <sanjeev1.785401@cct.tu.edu.np>
2025-02-02 22:48:52 +0545

    sign up page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home.jsx"
 import SignIn from "./pages/SignIn.jsx"
-import SignOut from "./pages/SignOut.jsx"
 import About from "./pages/About.jsx"
 import Profile from "./pages/Profile.jsx"
 import Header from "./components/Header.jsx"
+import SignUp from "./pages/SignUp.jsx"
 
 export default function App() {
   return (
@@ -13,7 +13,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-out" element={<SignOut />} />
+        <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
