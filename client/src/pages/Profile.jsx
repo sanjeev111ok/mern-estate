371c124e9adb55917c6c81acea04ad284bd1c9dd
Sanjeev Shrestha <sanjeev1.785401@cct.tu.edu.np>
2025-04-10 13:14:41 +0545

    create delete user functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -118,7 +118,10 @@ export default function Profile() {
   const handleShowListings = async () => {
     try {
       setShowListingsError(false)
-      const res = await fetch(`/api/user/listings/${currentUser._id}`)
+      const res = await fetch(`/api/user/listings/${currentUser._id}`, {
+        method: "GET",
+        credentials: "include",
+      })
       const data = await res.json()
       if (data.success === false) {
         setShowListingsError(true)
@@ -127,6 +130,23 @@ export default function Profile() {
       setUserListings(data)
     } catch (error) {
       setShowListingsError(true)
+    }
+  }
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      })
+      const data = await res.json()
+      if (data.success === false) {
+        console.log(data.message)
+        return
+      }
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id !== listingId)
+      )
+    } catch (error) {
+      console.error("Error deleting listing:", error)
     }
   }
   return (
@@ -219,7 +239,9 @@ export default function Profile() {
       </p>
       {userListings && userListings.length > 0 && (
         <div className="flex flex-col gap-4">
-          <h1 className="text-center mt-7 text-2xl font-semibold">Your Listings</h1>
+          <h1 className="text-center mt-7 text-2xl font-semibold">
+            Your Listings
+          </h1>
           {userListings.map((listing) => (
             <div
               className="border border-gray-500 rounded-lg p-3 flex justify-between items-center gap-4 "
@@ -239,7 +261,10 @@ export default function Profile() {
                 <p>{listing.name}</p>
               </Link>
               <div className="flex flex-col gap-2">
-                <button className="text-red-700 hover:underline uppercase">
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700 hover:underline uppercase"
+                >
                   Delete
                 </button>
                 <button className="text-green-700 hover:underline uppercase">
